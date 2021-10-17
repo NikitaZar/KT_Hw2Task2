bd2d63814f020ea41dd6e24a2c6a93fe3f3995f2
Nikita Zarubin <webbizon@ya.ru>
2021-10-17 16:35:43 +0400

    Corrected logic in getPeopleString method LikesCalc.kt class

--- src/main/kotlin/LikesCalc.kt
+++ src/main/kotlin/LikesCalc.kt
@@ -1,7 +1,9 @@
 class LikesCalc {
     companion object {
         fun getPeopleString(likes: Int): String {
-            return if (likes.mod(10) == 1) "$likes человеку" else "$likes людям"
+            return if (likes.mod(100) == 11) "$likes людям"
+            else if (likes.mod(10) == 1) "$likes человеку"
+            else "$likes людям"
         }
     }
 }
