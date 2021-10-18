0b917fe2af78e6f5a7ec2b97408783853bbb3b93
NikitaZar <nikita27051990@gmail.com>
2021-10-18 08:17:12 +0400

    Replace "if" to "when" operator in getPeopleString method LikesCalc.kt class

--- src/main/kotlin/LikesCalc.kt
+++ src/main/kotlin/LikesCalc.kt
@@ -1,9 +1,11 @@
 class LikesCalc {
     companion object {
         fun getPeopleString(likes: Int): String {
-            return if (likes.mod(100) == 11) "$likes людям"
-            else if (likes.mod(10) == 1) "$likes человеку"
-            else "$likes людям"
+            return when {
+                likes.mod(100) == 11 -> "$likes людям"
+                likes.mod(10) == 1 -> "$likes человеку"
+                else -> "$likes людям"
+            }
         }
     }
 }
